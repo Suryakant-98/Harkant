e3a240d81d31b40fac60162ef8040e655c9ea815
Suryakant Vijay <svijay@innominds.com>
2021-11-20 14:19:41 +0530

    login added

---
+++ Twitter/frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ForgotpasswordComponent } from './components/forgotpassword/forgotpassword.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+   
+    LoginComponent,
+    SignupComponent,
+    ForgotpasswordComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
